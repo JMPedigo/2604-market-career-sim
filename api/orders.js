1b9f40fb08121a5ec6f2f1e82b9e9e98a8d40546
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-10 14:31:30 -0500

    Adds middleware for router.param (getOrderById)

--- api/orders.js
+++ api/orders.js
@@ -3,7 +3,7 @@ const router = express.Router();
 export default router;
 
 import requireUser from "#middleware/requireUser";
-import { createOrder, getOrdersByUserId } from "#db/queries/orders";
+import { createOrder, getOrderById, getOrdersByUserId } from "#db/queries/orders";
 
 router.use(requireUser);
 
@@ -25,3 +25,17 @@ router.get("/", async (res, req) => {
 });
 
 /** Routing middleware that allows reuse of the logic for parsing ID parameter */
+router.param("/:id", (req,res, next, id) => {
+    const order = await getOrderById(id);
+    if (!order) return res.status(404).send("Order not found.");
+
+    if (req.order.user_id !== req.user.id) return res.status(403).send("You are not authorized to view this order.");
+
+    req.order = order;
+    next();
+});
+
+router.get("/:id", (req, res) => {
+    res.status(200).send(req.order);
+});
+
